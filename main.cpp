27275e6a8bc04d35f1f9d38ecddc2f40655e03bb
Daniil Bogdanov <51285775+exsandebest@users.noreply.github.com>
2020-01-04 15:16:08 +0300

    add parseNumber function

---
+++ main.cpp
@@ -0,0 +1,55 @@
+#include <string>
+#include <vector>
+using namespace std;
+
+struct token{
+    int type;
+    string value;
+    token(){
+        type = 0;
+        value = "";
+    }
+};
+
+string s;
+vector <token*> v;
+
+
+
+int parseNumber(int i){
+    string resValue = "";
+    token * res = new token();
+    while (isdigit(s[i])){
+        resValue += s[i];
+        ++i;
+    }
+    if (s[i] == '.'){
+        ++i;
+        if (isdigit(s[i])){
+            resValue += ".";
+            while (isdigit(s[i])){
+                resValue += s[i];
+                ++i;
+            }
+            res->value = resValue;
+            res->type = 2;
+            v.push_back(res);
+            return i;
+        } else {
+            throw "Incorrect number constant";
+        }
+    } else {
+        res->value = resValue;
+        res->type = 1;
+        v.push_back(res);
+        return i;
+    }
+}
+
+
+
+
+int main(){
+
+    return 0;
+}

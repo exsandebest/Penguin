9417206ae9f5d0891772f212545db1bcffc706d8
klimkomx <klimkomx@gmail.com>
2020-02-05 09:47:00 +0300

    added shit

--- Penguin.cpp
+++ Penguin.cpp
@@ -2,26 +2,37 @@
 #include <iostream>
 #include <string>
 
-using namespace std;
+#ifdef __unix__
+    
+     #define exe ""
+     #define prefix "./"
 
-int main(int argc, char const *argv[]){
+#elif defined(_WIN32) || defined(WIN32)
+
+     #define exe ".exe"
+     #define prefix ""
+
+#endif
+
+int main(int argc, char const *argv[]) {
     try{
         if (argc != 2) {
-            throw string("Incorrect arguments");
+            throw std::string("Incorrect arguments");
         }
-        string fileName = argv[1];
-        string tokensFile = "tokens.tmp";
-        if (ifstream(fileName)){
-            string startLA = "LexicalAnalyzer.exe -i " + fileName + " -o " + tokensFile;
+	
+	std :: string fileName = argv[1];
+	std :: string tokensFile = "tokens.tmp";
+        
+	if (std :: ifstream(fileName)){
+	    std :: string startLA = std :: string(prefix) + "LexicalAnalyzer" + std :: string(exe) + " -i " + fileName + " -o " + tokensFile;
             system(startLA.c_str());
-            string startSA = "SyntaxAnalyzer.exe " + tokensFile;
+	    std :: string startSA = std :: string(prefix) + "SyntaxAnalyzer" + std :: string(exe) + " " + tokensFile;
             system(startSA.c_str());
         } else {
-            throw string ("File does not exist");
+            throw std :: string ("File does not exist\n");
         }
-    } catch (string err){
-        cout << "Error: " << err;
-        return 0;
+    } catch (std :: string err) {
+	 std :: cout << "Error: " << err << "\n";
     }
     return 0;
 }

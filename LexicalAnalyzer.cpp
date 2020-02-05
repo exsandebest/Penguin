0768ec1fb23553df72c56896edcd60f022412c40
klimkomx <klimkomx@gmail.com>
2020-02-05 10:04:51 +0300

    refactor

--- LexicalAnalyzer.cpp
+++ LexicalAnalyzer.cpp
@@ -6,40 +6,38 @@
 #include <iostream>
 #include "Tokens.h"
 
-using namespace std;
-
-string s;
+std::string s;
 int line = 1;
 vector <Token*> v;
-vector <string> reservedWords = {"break", "continue", "if", "else", "return", "while", "for"};
-vector <string> reservedVariableTypes = {"bool", "string", "int", "double"};
-vector <string> reservedFunctionTypes = {"null"};
-vector <string> reservedOperators = {"and", "or", "xor"};
-vector <string> reservedFunctions = {"read", "write"};
-vector <string> reservedSpecialWords = {"import"};
+vector <std::string> reservedWords = {"break", "continue", "if", "else", "return", "while", "for"};
+vector <std::string> reservedVariableTypes = {"bool", "std::string", "int", "double"};
+vector <std::string> reservedFunctionTypes = {"null"};
+vector <std::string> reservedOperators = {"and", "or", "xor"};
+vector <std::string> reservedFunctions = {"read", "write"};
+vector <std::string> reservedSpecialWords = {"import"};
 
-void addToken(int type, string value);
+void addToken(int type, std::string value);
 bool ld (char c);
-bool detectReserved(string str, int i);
+bool detectReserved(std::string str, int i);
 int parseWord(int i);
 int parseNumber(int i);
 int parseString(int i);
-string deleteComments(string & str);
+std::string deleteComments(std::string & str);
 
-void addToken(int type, string value){
+void addToken(int type, std::string value) {
     Token * res = new Token(type, value, line);
     v.push_back(res);
 }
 
 
-bool ld (char c){
+bool ld (char c) {
     return (isdigit(c) || isalpha(c));
 }
 
 
-bool detectReserved(string str, int i){
-    string ts = "";
-    for (int j = i; j < i + str.length(); ++j){
+bool detectReserved(std::string str, int i) {
+    std::string ts = "";
+    for (int j = i; j < i + str.length(); ++j) {
         ts += s[j];
     }
     if (ts == str && !ld(s[i+str.length()])) {
@@ -50,38 +48,38 @@ bool detectReserved(string str, int i){
 }
 
 int parseWord(int i){
-    for (string word : reservedWords) {
+    for (std::string word : reservedWords) {
         if (detectReserved(word, i)) {
             addToken(7, word);
             return (i + word.length());
         }
     }
-    for (string word : reservedVariableTypes) {
+    for (std::string word : reservedVariableTypes) {
         if (detectReserved(word, i)) {
             addToken(5, word);
             return (i + word.length());
         }
     }
-    for (string word : reservedFunctionTypes) {
+    for (std::string word : reservedFunctionTypes) {
         if (detectReserved(word, i)) {
             addToken(6, word);
             return (i + word.length());
         }
     }
-    for (string word : reservedOperators) {
+    for (std::string word : reservedOperators) {
         if (detectReserved(word, i)) {
             addToken(8, word);
             return (i + word.length());
         }
     }
-    for (string word : reservedFunctions) {
+    for (std::string word : reservedFunctions) {
         if (detectReserved(word, i)) {
             addToken(19, word);
             return (i + word.length());
         }
     }
-    string ts = "";
-    while (ld(s[i])){
+    std::string ts = "";
+    while (ld(s[i])) {
         ts += s[i];
         ++i;
     }
@@ -91,17 +89,17 @@ int parseWord(int i){
 
 
 
-int parseNumber(int i){
-    string resValue = "";
-    while (isdigit(s[i])){
+int parseNumber(int i) {
+    std::string resValue = "";
+    while (isdigit(s[i])) {
         resValue += s[i];
         ++i;
     }
-    if (s[i] == '.'){
+    if (s[i] == '.') {
         ++i;
-        if (isdigit(s[i])){
+        if (isdigit(s[i])) {
             resValue += ".";
-            while (isdigit(s[i])){
+            while (isdigit(s[i])) {
                 resValue += s[i];
                 ++i;
             }
@@ -117,10 +115,10 @@ int parseNumber(int i){
 }
 
 
-int parseString(int i){
-    string resValue = "";
+int parseString(int i) {
+    std::string resValue = "";
     ++i;
-    while (s[i] != '"'){
+    while (s[i] != '"') {
         resValue += s[i];
         ++i;
     }
@@ -129,80 +127,80 @@ int parseString(int i){
     return i;
 }
 
-int parse(int i){
+int parse(int i) {
     if(i > s.length()-1) return -1;
-    string ts = "";
+    std::string ts = "";
 
-    if (s[i] == ';'){
+    if (s[i] == ';') {
         addToken(13,";");
         ++i;
         return parse(i);
     }
-    if (ts + s[i]+ s[i+1] == "==" || ts + s[i] + s[i+1] == "<=" || ts + s[i] + s[i+1] == ">=" || ts + s[i] + s[i+1] == "!="){
+    if (ts + s[i]+ s[i+1] == "==" || ts + s[i] + s[i+1] == "<=" || ts + s[i] + s[i+1] == ">=" || ts + s[i] + s[i+1] == "!=") {
         addToken(10, ts + s[i] + s[i+1]);
         return parse(i+2);
     }
-    if (s[i] == '='){
-        addToken(9, string(1, s[i]));
+    if (s[i] == '=') {
+        addToken(9, std::string(1, s[i]));
         return parse(i+1);
     }
-    if (s[i] == '~'){
-        addToken(12, string(1, s[i]));
+    if (s[i] == '~') {
+        addToken(12, std::string(1, s[i]));
         return parse(i+1);
     }
-    if (ts + s[i] + s[i+1] == ">>" || ts + s[i] + s[i+1] == "<<" || ts + s[i] + s[i+1] == "**"){
+    if (ts + s[i] + s[i+1] == ">>" || ts + s[i] + s[i+1] == "<<" || ts + s[i] + s[i+1] == "**") {
         addToken(11, ts + s[i] + s[i+1]);
         return parse(i+2);
     }
-    if (s[i] == '<' || s[i] == '>'){
-        addToken(10, string(1, s[i]));
+    if (s[i] == '<' || s[i] == '>') {
+        addToken(10, std::string(1, s[i]));
         return parse(i+1);
     }
-    if (ts + s[i] + s[i+1] == "++" || ts + s[i] + s[i+1] == "--"){
+    if (ts + s[i] + s[i+1] == "++" || ts + s[i] + s[i+1] == "--") {
         addToken(12, ts + s[i] + s[i+1]);
         return parse(i+2);
     }
-    if (s[i] == '+' || s[i] == '-' || s[i] == '/' || s[i] == '*' || s[i] == '%' || s[i] == '^' || s[i] == '|' || s[i] == '&'){
-        addToken(11, string(1,s[i]));
+    if (s[i] == '+' || s[i] == '-' || s[i] == '/' || s[i] == '*' || s[i] == '%' || s[i] == '^' || s[i] == '|' || s[i] == '&') {
+        addToken(11, std::string(1,s[i]));
         return parse(i+1);
     }
-    if (isdigit(s[i])){
+    if (isdigit(s[i])) {
         i = parseNumber(i);
         return parse(i);
     }
-    if (s[i] == '"'){
+    if (s[i] == '"') {
         i = parseString(i);
         return parse(i);
     }
-    if (isalpha(s[i])){
+    if (isalpha(s[i])) {
         i = parseWord(i);
         return parse(i);
     }
-    if (s[i] == '('){
+    if (s[i] == '(') {
         addToken(14, "(");
         return parse(i+1);
     }
-    if (s[i] == ')'){
+    if (s[i] == ')') {
         addToken(15, ")");
         return parse(i+1);
     }
-    if (s[i] == '{'){
+    if (s[i] == '{') {
         addToken(16, "{");
         return parse(i+1);
     }
-    if (s[i] == '}'){
+    if (s[i] == '}') {
         addToken(17, "}");
         return parse(i+1);
     }
-    if (s[i] == '['){
+    if (s[i] == '[') {
         addToken(20, "[");
         return parse(i+1);
     }
-    if (s[i] == ']'){
+    if (s[i] == ']') {
         addToken(21, "]");
         return parse(i+1);
     }
-    if (s[i] == ','){
+    if (s[i] == ',') {
         addToken(18, ",");
         return parse(i+1);
     }
@@ -210,19 +208,19 @@ int parse(int i){
         ++line;
         return parse(i+1);
     }
-    if (s[i] == ' ' || s[i] == '\r' || s[i] == '\t') return parse(i+1);
+    if (s[i] == ' ' || s[i] == '\r' || s[i] == '\t')
+        return parse(i+1);
 
-
-    throw string("Incorrect symbol : '" + string(1,s[i]) + "'\n");
+    throw std::string("Incorrect symbol : '" + std::string(1,s[i]) + "'\n");
 }
 
-string deleteComments(string & str){
-    string res = "", ts = "";
+std::string deleteComments(std::string & str) {
+    std::string res = "", ts = "";
     int i = 0;
     bool flag = 1;
     while (i < str.length()){
         if (ts + str[i] + str[i+1] == "*/" && flag){
-            throw string("Incorrect comments");
+            throw std::string("Incorrect comments");
         }
         if (ts + str[i] + str[i+1] == "/*" && flag){
             flag = 0;
@@ -239,7 +237,7 @@ string deleteComments(string & str){
         }
         ++i;
     }
-    if (!flag) throw string("Incorrect comments");
+    if (!flag) throw std::string("Incorrect comments");
     return res;
 }
 
@@ -248,24 +246,24 @@ int main(int argc, char const *argv[]){
     bool fromFile = false;
     try{
         if (argc % 2 == 0) {
-            throw string("Incorrect arguments");
+            throw std::string("Incorrect arguments");
         }
         for (int i = 1; i < argc; i += 2) {
             if (strcmp(argv[i], "-i") == 0) {
                 fromFile = true;
                 std :: ifstream inputFile(argv[i+1]);
                 if (!inputFile){
-                    throw string("Incorrect input file name");
+                    throw std::string("Incorrect input file name");
                 } else {
                     s.assign((std::istreambuf_iterator<char>(inputFile)),
                              (std::istreambuf_iterator<char>()));
                 }
             } else if (strcmp(argv[i], "-o") == 0) {
                 if (!freopen(argv[i+1], "w", stdout)) {
-                    throw string("Something wrong with the output file");
+                    throw std::string("Something wrong with the output file");
                 }
             } else {
-                throw string("Incorrect arguments");
+                throw std::string("Incorrect arguments");
             }
         }
         if (!fromFile) {
@@ -273,12 +271,12 @@ int main(int argc, char const *argv[]){
         }
         s = deleteComments(s);
         parse(0);
-    } catch (string err){
+    } catch (std::string err) {
         cout << err;
         return 0;
     }
     cout << v.size() << "\n";
-    for (int i = 0; i < v.size(); ++i){
+    for (int i = 0; i < v.size(); ++i) {
         cout << v[i]->line << "\n" << v[i]->type << "\n" << v[i]->size << "\n" << v[i]->value << "\n";
     }
     return 0;
--- Tokens.h
+++ Tokens.h
@@ -2,10 +2,11 @@
 
 using namespace std;
 
-struct Token{
+class Token {
+public:
     int type;
     string value;
     int size;
     int line;
-    Token(int type, string value, int line): type(type), value(value), size(value.size()), line(line){}
+    Token(int type, string value, int line): type(type), value(value), size(value.size()), line(line) {}
 };
